8641fa94b1c88682f5c2959db64bf612a6d44bb1
Jordi Cardoso <jordi.cardoso@bonpreu.cat>
2017-12-04 17:59:41 +0100

    Resultats to elasticsearch

--- src/test/java/cat/tcr/eleccions21d/simuladorTest.java
+++ src/test/java/cat/tcr/eleccions21d/simuladorTest.java
@@ -1,4 +1,4 @@
-package cat.tcr.simulador;
+package cat.tcr.eleccions21d;
 
 import static org.hamcrest.CoreMatchers.containsString;
 
@@ -6,6 +6,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
+import org.apache.camel.test.blueprint.*;
 import org.junit.Test;
 
 import java.io.InputStream;
@@ -31,14 +32,15 @@ public class simuladorTest extends CamelBlueprintTestSupport {
 	}
 
 	@Test
-	public void loadTest1() throws Exception {
+	public void totals2elasticTEST() throws Exception {
 		MockEndpoint result = getMockEndpoint(URI_END);
+
 		context.start();
 
 		result.expectedMessageCount(15);
-
 		result.assertIsSatisfied();
 
 		context.stop();
+
 	}
 }
